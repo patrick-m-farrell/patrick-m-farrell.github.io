ecbe2d62566fbfb202ba80ae6bec1cb43ce92686
patrick-m-farrell <pfarrell@risd.edu>
2023-10-16 23:27:21 -0400

    new_javascript_things

---
+++ main.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $('#rectangle-2').click(function() {
+        $('#rectangle-3').toggle();
+});
+                
+$('#rectangle-4').click(function() {
+    $('#rectangle-5').toggle();
+});
+});
+                
